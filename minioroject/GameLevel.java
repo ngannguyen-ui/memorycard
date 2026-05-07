861d95c6010bfb34e00582a270f7f14e9e85b253
ngannguyen-ui <kimngannguyen0708@gmail.com>
2026-05-07 10:34:52 +0700

    Add GameLevel enum

---
+++ minioroject/GameLevel.java
@@ -0,0 +1,28 @@
+interface DifficultyLevel{
+    int getPreviewTime();
+    int getMaxMoves();
+    String getRankName();
+}
+public enum GameLevel implements DifficultyLevel {
+    EASY(3000,30,"Beginner"),
+    MEDIUM(2000,20,"Intermediate"),
+    HARD(1000,10,"Expert");
+    private int previewTime;
+    private int maxMoves;
+    private String rankName;
+    GameLevel(int previewTime,int maxMoves,String rankName){
+        this.previewTime=previewTime;
+        this.maxMoves=maxMoves;
+        this.rankName=rankName;
+    }
+    public int getPreviewTime(){
+        return this.previewTime;
+    }
+    public int getMaxMoves(){
+        return this.maxMoves;
+    }
+    public String getRankName(){
+        return this.rankName;
+    }
+    
+}
