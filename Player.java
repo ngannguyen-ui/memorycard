9c727ec4044e5eaf2fdf7dd563b951217197a068
Pann2707 <ITDSIU25001@student.hcmiu.edu.vn>
2026-05-12 12:07:40 +0700

    fix + thêm leaderboard và để ng chơi tự đặt tên

--- Player.java
+++ Player.java
@@ -1,12 +1,39 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;;
+
 public class Player {
     private String Name;
     private int Score;
     private int matchedPairs;
     public Player(String Name){
-        this.Name=Name;
+        this.Name=playerName();
         this.Score=0;
         this.matchedPairs=0;
     }
+
+     private String generateRandomName(){
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 4; i++){
+            sb.append(chars.charAt(random.nextInt(chars.length())));
+        }
+        return sb.toString();
+    }
+
+    private String playerName(){
+        String name = JOptionPane.showInputDialog(
+            null,
+            "Input your name: ",
+            JOptionPane.PLAIN_MESSAGE
+        );
+        if (name == null || name.trim().isEmpty()){
+            name = generateRandomName();
+        }
+        return name.trim();
+    }
+
     public String getName(){
         return this.Name;
     }
@@ -22,4 +49,9 @@ public class Player {
     public void addMatchPair(Card currentCard,Card firstSeleCard){
         this.matchedPairs++;
         }
+    
+     @Override
+    public String toString(){
+        return Name + " | Score: " + Score;
+    }
 }
--- ScoreManager.java
+++ ScoreManager.java
@@ -1,7 +1,10 @@
+import java.util.*;
+
 public class ScoreManager {
     private int earnedPoints;
     private int BASE_SCORE;
     private int comboStack;
+    private List<Player> leaderboard = new ArrayList<>();
     public ScoreManager(){
         this.earnedPoints=0;
         this.comboStack=0;
@@ -32,6 +35,23 @@ public class ScoreManager {
     public void resetScore(){
         this.earnedPoints=0;
         this.comboStack=0;
+    }
+
+    public List<Player> getTopPlayers(int n){
+        return leaderboard.subList(0, Math.min(n, leaderboard.size()));
+    }
+
+      public void saveToLeaderboard(Player player) {
+        leaderboard.add(player);
+        leaderboard.sort(Comparator.comparingInt(Player::getScore).reversed());
+    }
+
+    public void printLearderboard(){
+        System.out.print("===  LEARDERBOARD ===\n");
+        List<Player> top = getTopPlayers(5);
+        for (int i =0; i < top.size(); i++){
+            System.out.println((i + 1) + "." + top.get(i));
+        }
     }
 }
     
